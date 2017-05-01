6589b851c261d06e4f0ebf22a4c28c88d7ce5b68
cyberplanner <sal@salweb.co.uk>
2017-05-01 15:43:30 +0100

    Add PORT env_variable and redirect https to http in server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express'); // require express
 var app     = express();          // create an app
-
+const PORT    = process.env.PORT || 4000; // set the port
+app.use(function(request, response, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));// Share a directory
 
 // listen to port 3000
-app.listen(4000, function() {
+app.listen(PORT, function() {
   // and log a msg to the console
-  console.log('express server is up on port 4000');
+  console.log('express server is up on port '+ PORT);
 });
