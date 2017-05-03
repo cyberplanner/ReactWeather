7e3d1ed19377e3ce3247944e32910a826f5f1b84
cyberplanner <sal@salweb.co.uk>
2017-05-03 11:40:42 +0100

    Add grid system and style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,5 @@
-var React = require('react');
-
+var React   = require('react');
+var { Link }= require('react-router');
 // var About = React.createClass({
 //   render: function() {
 //     return (
@@ -12,9 +12,20 @@ var React = require('react');
 
 var About = (props)=> {
   return (
-    <div>
-      <h3>About:</h3>
-      <p>Welcome to the about page!</p>
+    <div className="row">
+      <div className="columns medium-12 large-10 small-centered">
+        <h1 className="text-center">About:</h1>
+        <h4>Weather REACT project</h4>
+        <p>A simple app built with REACT and uses OpenWeatherMap's API to fetch and display weather.</p>
+        <a href="https://github.com/cyberplanner/ReactWeather">Github Repo</a> - Find the source code in this repo.
+        <div>
+          <h4><strong>Some of the tools I used:</strong></h4>
+          <ol>
+            <li><a href="https://facebook.github.io/react/">React</a></li> - Facebook's JS framework
+            <li><a href="http://openweathermap.org">Open Weather Map</a></li> - Open Weather Map Api
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
