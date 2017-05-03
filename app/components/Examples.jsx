8d1ed5953ead43e8d78501a8534a77a808c1d928
cyberplanner <sal@salweb.co.uk>
2017-05-03 10:26:58 +0100

    Fix typo in Links

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -14,12 +14,12 @@ var {Link} = require('react-router');
 var Examples = (probs) => {
   return (
     <div>
-      <h1 className="text-center">Examples:</h1>
+      <h1 className='text-center'>Examples:</h1>
       <p>Here are a few example locations:</p>
       <ol>
-        <li><link to="/?location=London,UK">London, UK</link></li>
-        <li><link to="/?location=New York City">New York City</link></li>
-        <li><link to="/?location=Cairo, EG">Cairo, EG</link></li>
+        <li><Link to='/?location=London,UK'>London, UK</Link></li>
+        <li><Link to='/?location=New York City'>New York City</Link></li>
+        <li><Link to='/?location=Cairo, EG'>Cairo, EG</Link></li>
       </ol>
     </div>
   );
